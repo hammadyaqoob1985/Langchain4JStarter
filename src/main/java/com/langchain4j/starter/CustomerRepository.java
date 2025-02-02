ec38feafedcd7d07cba9363dd717ba007d703a53
hammadyaqoob1985 <hammad.yaqoob1985@yahoo.co.uk>
2025-02-02 14:33:29 +0000

    Added startup data

--- src/main/java/com/langchain4j/starter/CustomerRepository.java
+++ src/main/java/com/langchain4j/starter/CustomerRepository.java
@@ -5,6 +5,10 @@ import com.langchain4j.starter.dto.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+    Optional<Customer> findCustomersByFirstNameAndLastName(String firstName, String lastName);
 }
---
+++ src/main/java/com/langchain4j/starter/config/DataLoader.java
@@ -0,0 +1,85 @@
+package com.langchain4j.starter.config;
+
+
+import com.langchain4j.starter.BookingRepository;
+import com.langchain4j.starter.CustomerRepository;
+import com.langchain4j.starter.dto.Booking;
+import com.langchain4j.starter.dto.BookingClass;
+import com.langchain4j.starter.dto.BookingStatus;
+import com.langchain4j.starter.dto.Customer;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
+
+@Component
+
+public class DataLoader {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataLoader.class);
+    private final CustomerRepository customerRepository;
+    private final BookingRepository bookingRepository;
+
+    public DataLoader(CustomerRepository customerRepository, BookingRepository bookingRepository) {
+        this.customerRepository = customerRepository;
+        this.bookingRepository = bookingRepository;
+    }
+
+
+    @PostConstruct
+    public void init() {
+        Customer johnCustomer = new Customer();
+        johnCustomer.setFirstName("John");
+        johnCustomer.setLastName("Doe");
+
+        Customer janeCustomer = new Customer();
+        janeCustomer.setFirstName("Jane");
+        janeCustomer.setLastName("Smith");
+
+        Customer michaelCustomer = new Customer();
+        michaelCustomer.setFirstName("Michael");
+        michaelCustomer.setLastName("Johnson");
+
+        Customer sarahCustomer = new Customer();
+        sarahCustomer.setFirstName("Sarah");
+        sarahCustomer.setLastName("Williams");
+
+        Customer robertCustomer = new Customer();
+        robertCustomer.setFirstName("Robert");
+        robertCustomer.setLastName("Taylor");
+
+        customerRepository.saveAll(List.of(johnCustomer, janeCustomer, michaelCustomer, sarahCustomer, robertCustomer));
+
+
+       customerRepository.findCustomersByFirstNameAndLastName("John", "Doe").ifPresent(this::generateBookingForCustomer);
+       customerRepository.findCustomersByFirstNameAndLastName("Jane", "Smith").ifPresent(this::generateBookingForCustomer);
+       customerRepository.findCustomersByFirstNameAndLastName("Michael", "Johnson").ifPresent(this::generateBookingForCustomer);
+       customerRepository.findCustomersByFirstNameAndLastName("Sarah", "Williams").ifPresent(this::generateBookingForCustomer);
+       customerRepository.findCustomersByFirstNameAndLastName("Robert", "Taylor").ifPresent(this::generateBookingForCustomer);
+
+
+    }
+
+    private void generateBookingForCustomer(Customer customer) {
+        List<String> airportCodes = List.of("LAX", "SFO", "JFK", "LHR", "CDG", "ARN", "HEL", "TXL", "MUC", "FRA", "MAD", "SJC");
+        Random random = new Random();
+        Booking booking = new Booking();
+        booking.setCustomer(customer);
+        booking.setBookingClass(BookingClass.values()[random.nextInt(BookingClass.values().length)]);
+        booking.setBookingStatus(BookingStatus.CONFIRMED);
+        LocalDate bookingDate = LocalDate.now().plusDays(random.nextInt());
+        booking.setDate(bookingDate);
+        booking.setBookingTo(bookingDate.plusDays(7));
+        booking.setSource(airportCodes.get(random.nextInt(airportCodes.size())));
+        booking.setDestination(airportCodes.get(random.nextInt(airportCodes.size())));
+        Booking savedBooking = bookingRepository.save(booking);
+        customer.setBookings(List.of(savedBooking));
+        customerRepository.save(customer);
+
+    }
+
+}
